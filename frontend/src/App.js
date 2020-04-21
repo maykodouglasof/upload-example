d5e20b0d66cf835a3d12b4df5e9c772224d92b2f
maykodouglasof <maykodouglasbr@outlook.com>
2020-04-20 22:14:56 -0300

    Atualização nome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -107,6 +107,7 @@ class App extends Component {
     return (
       <Container>
         <Content>
+        <h1>Enviar Receita</h1>
           <Upload onUpload={this.handleUpload} />
           {!!uploadedFiles.length && (
             <FileList files={uploadedFiles} onDelete={this.handleDelete} />
